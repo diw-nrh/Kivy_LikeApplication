ca28b36911579fe1a7e9cfb4ebd20f894e21998a
wanfadiw <fadiwmimi14@gmail.com>
2025-01-01 20:28:24 +0700

    updatefile
    Day 1 test 2

---
+++ project_kivy_SD.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget): 
+    pass
+
+class ThelapApp(App):
+    pass
+
+if __name__ == '__main__':
+    ThelapApp().run()
