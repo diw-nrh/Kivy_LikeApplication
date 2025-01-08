5a757fdad3ae1dc4fd80840f2468a2933d72f4da
wanfadiw <fadiwmimi14@gmail.com>
2025-01-08 20:29:54 +0700

    Update project_kivy_SD.py

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -6,10 +6,18 @@ from kivy.core.window import Window
 class GameWidget(Widget):
     def __init__(self, **kwargs):
         super(GameWidget, self).__init__(**kwargs)
-        self._keybord = Window.request_keyboard(self._on_keyboard_closed, self)
+        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_key_down)  # Corrected here
+
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        print("Key Down has been pressed")
 
         with self.canvas:
-            Rectangle(source="",pos=(0, 0), size=(110, 100))
+            Rectangle(source="D:\code_vs_stady\PYTHON_TEST_STUDY\Application_study\LAP_KIVY\LAP_kv_4\download.jpg", pos=(0, 0), size=(110, 100))
 
 class MyApp(App):
     def build(self):
