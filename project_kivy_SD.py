bfb01691672b4217192af5a2c8c4a7130f6d46a2
wanfadiw <fadiwmimi14@gmail.com>
2025-01-08 20:27:37 +0700

    Update project_kivy_SD.py

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle
+from kivy.core.window import Window
 
 class GameWidget(Widget):
     def __init__(self, **kwargs):
         super(GameWidget, self).__init__(**kwargs)
+        self._keybord = Window.request_keyboard(self._on_keyboard_closed, self)
 
         with self.canvas:
             Rectangle(source="",pos=(0, 0), size=(110, 100))
