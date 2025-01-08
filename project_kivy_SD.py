a8d2438d1c195272416f777ebd0f020ffb0fb068
wanfadiw <fadiwmimi14@gmail.com>
2025-01-08 20:24:32 +0700

    Update project_kivy_SD.py

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -5,9 +5,9 @@ from kivy.graphics import Color, Rectangle
 class GameWidget(Widget):
     def __init__(self, **kwargs):
         super(GameWidget, self).__init__(**kwargs)
+
         with self.canvas:
-            Color(1, 0, 0, 1)
-            self.rect = Rectangle(pos=(0, 0), size=(110, 100))
+            Rectangle(source="",pos=(0, 0), size=(110, 100))
 
 class MyApp(App):
     def build(self):
