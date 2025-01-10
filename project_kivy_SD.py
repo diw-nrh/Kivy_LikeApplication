eca372593661ff52bc719b25f3bb8a15ddbdb366
wanfadiw <fadiwmimi14@gmail.com>
2025-01-10 20:19:59 +0700

    Update project_kivy_SD.py

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -2,6 +2,11 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout  
+
+
+class AnchorLayouExample(AnchorLayout):
+    
 
 class BoxLayoutExample(BoxLayout):
     pass
