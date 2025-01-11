230953ce8fef9b1efc36c49ed5a67cc7cc72b426
wanfadiw <fadiwmimi14@gmail.com>
2025-01-11 13:01:09 +0700

    test range 100

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -10,8 +10,8 @@ from kivy.metrics import dp
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = "lr-bt"
-        for i in range(0, 10):
+        # self.orientation = "lr-bt"
+        for i in range(0, 100):
             size = dp(100)
             b = Button(
                 text=str(i + 1),
