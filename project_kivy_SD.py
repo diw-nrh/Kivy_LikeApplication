d7ceef0e09a43340c66153bc079e0ba50e206d2c
wanfadiw <fadiwmimi14@gmail.com>
2025-01-12 13:26:34 +0700

    Update project_kivy_SD.py

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -7,17 +7,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.metrics import dp
 
-class StackLayoutExample(StackLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # self.orientation = "lr-bt"
-        for i in range(0, 100):
-            size = dp(100)
-            b = Button(
-                text=str(i + 1),
-                size_hint=(None, None),
-                size=(size, size))
-            self.add_widget(b)
+class WidgetsExample(GridLayout):
+    my_test = StringProperty("Hello")
 
 # class GridLayouExample(GridLayout):
 #     pass
