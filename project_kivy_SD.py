af817aad0abcd80f4b18b6a525ef1db8ccfa3523
wanfadiw <fadiwmimi14@gmail.com>
2025-01-12 13:39:42 +0700

    test program

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -1,41 +1,17 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout  
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.stacklayout import StackLayout
-from kivy.metrics import dp
+from kivy.properties import StringProperty
 
 class WidgetsExample(GridLayout):
     my_test = StringProperty("Hello")
+
     def on_button_click(self):
         print("Button clicked")
-
-# class GridLayouExample(GridLayout):
-#     pass
-
-# class AnchorLayouExample(AnchorLayout):
-#     pass
-
-class BoxLayoutExample(BoxLayout):
-    pass
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     self.orientation = 'vertical'
-    #     b1 = Button(text='A')
-    #     b2 = Button(text='B')
-    #     b3 = Button(text='C')
-    #     self.add_widget(b1)
-    #     self.add_widget(b2)
-    #     self.add_widget(b3)
-
-class MainWidget(Widget): 
-    pass
+        self.my_test = "You Clicked"
 
 class ThelapApp(App):
     def build(self):
-        return StackLayoutExample()
+        return WidgetsExample()
 
 if __name__ == '__main__':
     ThelapApp().run()
