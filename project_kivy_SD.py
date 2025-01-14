95a7ddffd6592f0574fa316c5e6b1010f7a3308a
wanfadiw <fadiwmimi14@gmail.com>
2025-01-14 11:34:33 +0700

    Update project_kivy_SD.py

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -4,18 +4,16 @@ from kivy.properties import StringProperty, BooleanProperty
 
 
 class WidgetsExample(GridLayout):
-    count = 1  # ตัวนับเริ่มต้น
-    count_enabled = BooleanProperty(False)  # ตัวแปรสำหรับเปิด/ปิดปุ่ม Count
-    my_test = StringProperty("1")  # เก็บค่าที่จะแสดงผลใน Label
+    count = 1
+    count_enabled = BooleanProperty(False)
+    my_test = StringProperty("1")
 
     def on_button_click(self):
-        # ฟังก์ชันเมื่อกดปุ่ม Count
         if self.count_enabled:
             self.count += 1
             self.my_test = str(self.count)
 
     def on_toggle_button_state(self, widget):
-        # ฟังก์ชันเมื่อ ToggleButton เปลี่ยนสถานะ
         if widget.state == "normal":
             widget.text = "OFF"
             self.count_enabled = False
@@ -24,11 +22,9 @@ class WidgetsExample(GridLayout):
             self.count_enabled = True
 
     def on_switch_active(self, widget):
-        # ฟังก์ชันเมื่อ Switch ถูกเปิด/ปิด
         print(f"Switch active: {widget.active}")
 
     def on_slider_value_change(self, widget):
-        # ฟังก์ชันเมื่อค่า Slider เปลี่ยน
         print(f"Slider value: {widget.value}")
 
 
