214b0e09f87520ee74caa8973e9d91c66b8935da
wanfadiw <fadiwmimi14@gmail.com>
2025-01-20 16:01:25 +0700

    test game

--- project_kivy_SD.py
+++ project_kivy_SD.py
@@ -1,38 +1,17 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.clock import Clock
+from kivy.lang import Builder
+import random
 
-
-class WidgetsExample(GridLayout):
-    count = 1
-    count_enabled = BooleanProperty(False)
-    my_test = StringProperty("1")
-
-    def on_button_click(self):
-        if self.count_enabled:
-            self.count += 1
-            self.my_test = str(self.count)
-
-    def on_toggle_button_state(self, widget):
-        if widget.state == "normal":
-            widget.text = "OFF"
-            self.count_enabled = False
-        else:
-            widget.text = "ON"
-            self.count_enabled = True
-
-    def on_switch_active(self, widget):
-        print(f"Switch active: {widget.active}")
-
-    def on_slider_value_change(self, widget):
-        print(f"Slider value: {widget.value}")
-
-
-class ThelapApp(App):
+class WhackAMoleApp(App):
     def build(self):
-        return WidgetsExample()
+        Builder.load_file("whackamole.kv")
+        self.score = 0
+        self.moles = []
+        self.active_mole = None
+        self.mole_speed = 1.0
+        self.clock_event = None
+        return self.root
 
-
-if __name__ == '__main__':
-    ThelapApp().run()
-
+if __name__ == "__main__":
+    WhackAMoleApp().run()
